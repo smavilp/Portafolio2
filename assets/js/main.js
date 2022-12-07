1cf2b47f7c7871115ea19ed5332c075c6b01a5c9
smavilp <smavilp@gmail.com>
2022-12-06 23:02:03 -0500

    funcionalidad del menu del mobil

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
 const menuIcon = document.getElementById("menu-btn");
 const closeIcon = document.getElementById("close-btn");
 const menu = document.getElementById("menu-div");
-const aboutMeAncore = document.getElementById("about-me-section");
+const aboutMeAncore = document.getElementById("about-me-ancore");
+const myHabilitiesAncore = document.getElementById("my-habilities-ancore");
+const formationAncore = document.getElementById("formation-ancore");
+const projectsAncore = document.getElementById("projects-ancore");
+const contactAncore = document.getElementById("contact-ancore");
 
 const openMenu = () => {menu.classList.remove("hide-menu")};
 const closeMenu = () => {menu.classList.add("hide-menu")};
 
 menuIcon.addEventListener ("click", e => openMenu());
 closeIcon.addEventListener ("click", e => closeMenu());
+
 aboutMeAncore.addEventListener ("click", e => closeMenu());
+myHabilitiesAncore.addEventListener ("click", e => closeMenu());
+formationAncore.addEventListener ("click", e => closeMenu()); 
+projectsAncore.addEventListener ("click", e => closeMenu()); 
+contactAncore.addEventListener ("click", e => closeMenu());  
